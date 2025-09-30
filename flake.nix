5a1d9d2e66f09dae0250632edec1c3ad8168cc4a
Stan-breaks <stanleymwendwa03@gmail.com>
2025-09-30 13:43:08 +0300

    feat: added nix support

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "ytsurf - YouTube terminal search and playback tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "ytsurf";
+        version = "2.0.1"; # update when you tag releases
+
+        src = ./.;
+
+        buildInputs = [
+          pkgs.bash
+          pkgs.yt-dlp
+          pkgs.jq
+          pkgs.curl
+          pkgs.mpv
+          pkgs.fzf
+          pkgs.chafa
+          pkgs.ffmpeg
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ytsurf.sh $out/bin/ytsurf
+          chmod +x $out/bin/ytsurf
+        '';
+      };
+
+      # Allows `nix run .`
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/ytsurf";
+      };
+    };
+}
