9c3bb4ce14175cd83f32c882f4510b837293ea69
Stan-breaks <stanleymwendwa03@gmail.com>
2025-12-17 00:32:26 +0300

    chore: version update

--- ytsurf.rb
+++ ytsurf.rb
@@ -2,7 +2,7 @@ class Ytsurf < Formula
   desc "YouTube in your terminal. Clean and distraction-free"
   homepage ""
   url "https://github.com/Stan-breaks/ytsurf/archive/refs/tags/v3.0.4.zip"
-  sha256 "5714196c2b6f48f496181a9cde334feb3d7585df4bc5ddb02770ac715b5acb66"
+  sha256 "d72e25f8bd9c9998f454f5455acc940e7aeee6de5916b19f5730f558ef6f189a"
   version "3.0.4"
   license "GPL-3.0"
   
