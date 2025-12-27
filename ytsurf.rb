4fd95eb0e789a6f4f7bef0b829cedee09243f806
Stan-breaks <stanleymwendwa03@gmail.com>
2025-12-27 10:37:07 +0300

    chore: checksum update

--- ytsurf.rb
+++ ytsurf.rb
@@ -2,7 +2,7 @@ class Ytsurf < Formula
   desc "YouTube in your terminal. Clean and distraction-free"
   homepage ""
   url "https://github.com/Stan-breaks/ytsurf/archive/refs/tags/v3.0.7.zip"
-  sha256 "82d319f42e20754b1fc325852243a68a29e32bb54c81e5595e2c3282a2e6c1ec"
+  sha256 "d35443223f95ba97bfe90c8eb4b35d8f23b9c2276a6886471b7a306fd9c2016b"
   version "3.0.7"
   license "GPL-3.0"
   
