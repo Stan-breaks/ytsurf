67386df002966f3183422a8fd4e5961e7a98758a
Stan-breaks <stanleymwendwa03@gmail.com>
2026-01-10 12:21:21 +0300

    chore: remove debuging

--- ytsurf.sh
+++ ytsurf.sh
@@ -496,7 +496,6 @@ subscribe(){
     selected_item=$(printf "%s\n" "${menu_items[@]}" | sed 's/ /␣/g' | sentaku)
     selected_item=${selected_item//␣/ }
   else
-    echo "$jsonData"
     previewScript=$(create_preview_script_fzf_channel)
     selected_item=$(printf "%s\n" "${menuList[@]}" | fzf \
           --prompt="search channel" \
