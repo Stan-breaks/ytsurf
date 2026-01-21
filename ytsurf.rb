72500d84872cd7466970f1c4d400640ab1b33c64
Stan-breaks <stanleymwendwa03@gmail.com>
2026-01-21 21:07:54 +0300

    chore: version update

--- ytsurf.rb
+++ ytsurf.rb
@@ -1,9 +1,9 @@
 class Ytsurf < Formula
   desc "YouTube in your terminal. Clean and distraction-free"
   homepage ""
-  url "https://github.com/Stan-breaks/ytsurf/archive/refs/tags/v3.0.10.zip"
+  url "https://github.com/Stan-breaks/ytsurf/archive/refs/tags/v3.1.0.zip"
   sha256 "99245b495783ab8d553fa4c0121e1a5370b6a54a19f7b0583c61c7b4dbad4eb6"
-  version "3.0.10"
+  version "3.1.0"
   license "GPL-3.0"
   
   depends_on "bash"
--- ytsurf.sh
+++ ytsurf.sh
@@ -5,7 +5,7 @@ set -u
 # CONSTANTS AND DEFAULTS
 #=============================================================================
 
-readonly SCRIPT_VERSION="3.0.10"
+readonly SCRIPT_VERSION="3.1.0"
 readonly SCRIPT_NAME="ytsurf"
 
 # Default configuration values
