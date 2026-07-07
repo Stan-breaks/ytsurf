11f7b1806e3fd1e638cd68e4fc21f301889afb20
Stan-breaks <stanleymwendwa03@gmail.com>
2026-07-07 11:42:55 +0300

    fix: unbound nofication error

--- ytsurf.sh
+++ ytsurf.sh
@@ -362,9 +362,9 @@ update_script() {
     send_notification "Script is up to date :)"
   else
     if printf '%s\n' "$update" | patch "$which_ytsurf" -; then
-      send_notification "$SCRIPT_NAME" "Script has been updated!"
+      send_notification "Error" "Script has been updated!"
     else
-      send_notification "$SCRIPT_NAME" "Can't update for some reason! update with Paru or yay if on archlinux"
+      send_notification "Error" "Can't update for some reason! update with Paru or yay if on archlinux"
     fi
   fi
   exit 0
